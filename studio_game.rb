9bce50cd655e07c27a93f4f9e6c41eae9f2748f0
Michael Westphal <michaelwestphal@hey.com>
2021-09-28 09:53:21 -0500

    Chapter 4: Variables and Objects

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,28 @@
-greeting = "Welcome!"
+name1 = 'larry'
+health1 = 60
 
-3.times do
-    puts greeting.upcase
-end
+name2 = 'curly'
+health2 = 125
 
-puts Time.new
+name3 = 'moe'
+health3 = 100
+
+name4 = 'shemp'
+health4 = 90
+
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+
+health2 = health1
+puts "#{name2.upcase} has a health of #{health2}."
+
+health1 = 30
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
+
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+
+current_formatted_time = Time.new.strftime('%A %m/%d/%Y at %H:%M')
+puts "The game started on #{current_formatted_time}"
