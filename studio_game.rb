eea11c2f7e5eb74b4b78fc8f140e8345c0329dff
Michael Westphal <michaelwestphal@hey.com>
2021-09-28 10:34:20 -0500

    Chapter 6: Methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,28 +1,16 @@
-name1 = 'larry'
-health1 = 60
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+end
 
-name2 = 'curly'
-health2 = 125
+def time
+  current_time = Time.new
+  current_time.strftime('%H:%M:%S')
+end
 
-name3 = 'moe'
-health3 = 100
-
-name4 = 'shemp'
-health4 = 90
-
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
-
-health2 = health1
-puts "#{name2.upcase} has a health of #{health2}."
-
-health1 = 30
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
-
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
-
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+puts say_hello('larry', 60)
+puts say_hello('curly', 125)
+puts say_hello('moe')
+puts say_hello('shemp', 90)
 
 current_formatted_time = Time.new.strftime('%A %m/%d/%Y at %H:%M')
 puts "The game started on #{current_formatted_time}"
