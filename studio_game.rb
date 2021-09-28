497d391f00e37c37aae2a0a3c39240fd85298d0d
Michael Westphal <michaelwestphal@hey.com>
2021-09-28 15:12:17 -0500

    Chapter 8: Attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,7 +1,14 @@
 class Player
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
   end
 
   def blam
@@ -14,8 +21,12 @@ class Player
     puts "#{@name} got w00ted!"
   end
 
+  def score
+    @health + @name.length
+  end
+
   def to_s
-    "I'm #{@name} with a health of #{@health} as of #{self.time}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def time
@@ -31,13 +42,18 @@ player1 = Player.new('moe')
 puts player1
 
 player2 = Player.new('larry', 60)
+puts player2.name
+player2.name = 'lawrence'
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
 
-player3 = Player.new('curly', 125)
-puts player3
-player3.blam
-puts player3
-player3.w00t
-puts player3
+# player3 = Player.new('curly', 125)
+# puts player3
+# player3.blam
+# puts player3
+# player3.w00t
+# puts player3
 
 # puts player3.inspect
