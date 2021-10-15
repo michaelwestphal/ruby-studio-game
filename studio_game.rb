4e4ec55f2b3e60b7e46bd0de03f4288ae4b2e337
Michael Westphal <michaelwestphal@hey.com>
2021-10-15 07:59:39 -0500

    Chapter 10: Objects interacting

--- studio_game.rb
+++ studio_game.rb
@@ -35,29 +35,45 @@ class Player
   end
 end
 
-current_formatted_time = Time.new.strftime('%A %m/%d/%Y at %H:%M')
-puts "The game started on #{current_formatted_time}"
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+    # Or with push
+    # @players.push(player)
+  end
+
+  def play
+    current_formatted_time = Time.new.strftime('%A %m/%d/%Y at %H:%M')
+    puts "The game started on #{current_formatted_time}"
+    
+    puts "There are #{@players.size} players in #{@title}:"
+    puts @players
+  end
+end
+
 
 player1 = Player.new('moe')
 player2 = Player.new('larry', 60)
 player3 = Player.new('curly', 125)
 
-players = [player1, player2, player3]
+knuckleheads = Game.new('Knuckleheads')
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
 
-puts "There are #{players.size} players in the game:"
-
-players.each do |player|
-  player.blam
-  player.w00t
-  player.w00t
-  puts player
-end
-
-players.pop
-players.push(Player.new('shemp', 90))
-
-players.each do |player|
-  puts player
-end
+# players.each do |player|
+#   player.blam
+#   player.w00t
+#   player.w00t
+#   puts player
+# end
 
 # puts player3.inspect
